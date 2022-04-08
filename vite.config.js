8b95760e1887c4ddc5a5fe604b81804e368fb01a
RK Anik <rkanik.me@gmail.com>
2022-04-08 22:11:29 +0600

    removed stylus

--- vite.config.js
+++ vite.config.js
@@ -5,31 +5,44 @@ import { defineConfig } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 
 export default defineConfig({
-  plugins: [createVuePlugin(), visualizer(), eslintPlugin()],
-  server: {
-    port: 8080,
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
-    ],
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.css'],
-  },
-  css: {
-    preprocessorOptions: {
-      sass: {
-        additionalData: [
-          '@import "@/assets/scss/_variables.scss"',
-          '@import "@/assets/scss/_mixins.scss"',
-          '',
-        ].join('\n'),
-      },
-    },
-  },
-  build: {
-    chunkSizeWarningLimit: 700,
-  },
+	plugins: [
+		createVuePlugin(),
+		eslintPlugin(),
+		visualizer({
+			open: true,
+			filename: 'stats.html',
+			title: 'Rollup Visualizer',
+			template: 'treemap',
+			json: false,
+			gzipSize: false,
+			brotliSize: false,
+			projectRoot: process.cwd(),
+		}),
+	],
+	server: {
+		port: 8080,
+	},
+	resolve: {
+		alias: [
+			{
+				find: '@',
+				replacement: path.resolve(__dirname, 'src'),
+			},
+		],
+		extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue', '.css'],
+	},
+	css: {
+		preprocessorOptions: {
+			sass: {
+				additionalData: [
+					'@import "@/assets/scss/_variables.scss"',
+					'@import "@/assets/scss/_mixins.scss"',
+					'',
+				].join('\n'),
+			},
+		},
+	},
+	build: {
+		chunkSizeWarningLimit: 700,
+	},
 });
